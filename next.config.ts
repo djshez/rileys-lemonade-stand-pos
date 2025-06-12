933caa93993d7eb7cd2f0954a9490f4f27d6d9bc
djshez <57478012+djshez@users.noreply.github.com>
2025-06-12 11:52:36 -0500

    Add next.config.ts 1749747131502

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
